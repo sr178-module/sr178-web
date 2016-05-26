6ee536b10ee36793d171b8f20065481e3e1aeffa
dogdog7788 <dogdog7788@qq.com>
2016-05-26 18:12:06 +0800

    感叹号

--- src/main/java/com/sr178/module/web/interceptor/JsonExceptionInterceptor.java
+++ src/main/java/com/sr178/module/web/interceptor/JsonExceptionInterceptor.java
@@ -1,8 +1,5 @@
 package com.sr178.module.web.interceptor;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 
 import com.opensymphony.xwork2.ActionInvocation;
@@ -26,22 +23,6 @@ public class JsonExceptionInterceptor extends BaseInterceptor {
 	public String intercept(ActionInvocation invocation) throws Exception {
 		
 		JsonBaseActionSupport jsonAction = (JsonBaseActionSupport) invocation.getAction();
-		// sql注入检查
-//		String checkResult = checkSql(invocation);
-//		if (checkResult != null) {
-//			String msg = getMsgTag() + "[userName]=[" + jsonAction.getLoginUser()+ "]";
-//			jsonAction.renderErrorResult(WebError.GLOAB_ERROR_SQL, "注入==》关键字="+checkResult);
-//			LogSystem.warn("注入异常==》"+msg);
-//			return JsonBaseActionSupport.JSON;
-//		}
-		
-//		if(!jsonAction.isCanVisite()){
-//			String msg = getMsgTag() + "[userName]=[" + aldAction.getUserName() + "]";
-//			aldAction.setCode(WebError.GLOAB_ERROR_VISIT_LIMIT);
-//			aldAction.setDesc("并发访问限制：");
-//			LogSystem.warn("并发访问异常==》"+msg);
-//			return WebError.GLOAB_ERROR_RESULT;
-//		}
 		String userName = jsonAction.getLoginUser();
 		try {
 			return invocation.invoke();
