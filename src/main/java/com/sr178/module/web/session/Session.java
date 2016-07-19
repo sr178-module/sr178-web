b874a49de00ab657ee9d5f2f36084f2e0c8a55c0
dogdog7788 <dogdog7788@qq.com>
2016-07-19 13:33:16 +0800

    加入属相map

--- src/main/java/com/sr178/module/web/session/Session.java
+++ src/main/java/com/sr178/module/web/session/Session.java
@@ -1,5 +1,8 @@
 package com.sr178.module.web.session;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Session {
 	private String sessionId;
 	
@@ -10,6 +13,9 @@ public class Session {
 	private int badVisitTimes;
 	//登录时间
 	private long loginTime;
+	//属性表
+	private Map<String,Object> attr = new HashMap<String,Object>();
+	
 	public String getUserName() {
 		return userName;
 	}
@@ -67,4 +73,29 @@ public class Session {
 		this.userName = userName;
 	}
 	
+	public void setAttr(String key,Object value){
+		attr.put(key, value);
+	}
+	
+	public Object getAttr(String key){
+		return attr.get(key);
+	}
+	
+	public String getStringAttr(String key){
+		return (String)attr.get(key);
+	}
+	
+	public Integer getIntAttr(String key){
+		if(attr.containsKey(key)){
+			return (Integer)attr.get(key);
+		}
+		return null;
+	}
+	
+	public Double getDoubleAttr(String key){
+		if(attr.containsKey(key)){
+			return (Double)attr.get(key);
+		}
+		return null;
+	}
 }
